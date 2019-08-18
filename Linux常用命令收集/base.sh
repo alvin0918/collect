dfb8d5060b890204403b894994b68cef197136cc
Chen.Yang <15517127859@163.com>
2019-08-18 15:32:13 +0800

    *

--- Linux常用命令收集/base.sh
+++ Linux常用命令收集/base.sh
@@ -2,3 +2,12 @@
 
 # 统计某一天网站的访问量
 awk '{print $1}' /var/log/access.log | sort | uniq | wc -l
+
+# 查看物理cpu个数
+grep 'physical id' /proc/cpuinfo | sort -u
+
+# 查看核心数量
+grep 'core id' /proc/cpuinfo | sort -u | wc -l
+
+# 查看线程数
+grep 'processor' /proc/cpuinfo | sort -u | wc -l
