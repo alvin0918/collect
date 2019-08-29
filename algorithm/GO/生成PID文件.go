c3564c38555f5d0590b212e5b26b9b27c183ece0
Chen.Yang <15517127859@163.com>
2019-08-29 09:56:45 +0800

    *

---
+++ algorithm/GO/生成PID文件.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"syscall"
+)
+
+func initPid() (once_pid int, started bool) {
+
+	var pidfile string = ".pid"
+
+	pf, err := os.OpenFile(pidfile, os.O_RDWR, 0)
+	defer pf.Close()
+	if os.IsNotExist(err) {
+		started = false
+	} else if err != nil {
+		fmt.Printf("pidfile check error:%v\n", err)
+		return
+	} else {
+		pd, _ := ioutil.ReadAll(pf)
+		old_pid, err := strconv.Atoi(string(pd))
+		if err == nil {
+			err := syscall.Kill(old_pid, 0)
+			if err == nil {
+				started = true
+				once_pid = old_pid
+			}
+		} else {
+			return
+		}
+	}
+	if !started {
+		pf, err := os.Create(pidfile)
+		if err != nil {
+			fmt.Println("create pid file error.")
+			return
+		}
+		new_pid := os.Getpid()
+		_, err = pf.Write([]byte(fmt.Sprintf("%d", new_pid)))
+		if err != nil {
+			fmt.Println("write pid failed.")
+		} else {
+			once_pid = new_pid
+		}
+	}
+	return
+}
+
+func main()  {
+	initPid()
+}
